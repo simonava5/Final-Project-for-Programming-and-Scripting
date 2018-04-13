d3e6b46064c1784d0f88ef7208d92371b1199303
Simona Vasiliauskaite <G00263352@gmit.ie>
2018-04-13 22:11:59 +0100

    listing all data from the first column

--- ID1.py
+++ ID1.py
@@ -8,7 +8,8 @@ import numpy
 #use this function to open iris.data.csv which is saved on my device and separate all values with a coma delimiter
 data = numpy.genfromtxt('iris.data.csv', delimiter=',')
 
-
+#listing all data from the first column of the data set which is the sepal length 
+firstcol = data[:,0]
 
 
 
