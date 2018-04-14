fd50b4273cb234d04b6e5c68fe0c266e36ddec64
Simona Vasiliauskaite <G00263352@gmit.ie>
2018-04-14 11:46:07 +0100

    Imported modules and functions

--- Irisdata1.py
+++ Irisdata1.py
@@ -2,23 +2,20 @@
 # 13/04/2018
 # Calculating the mean of each column
 
-# imported following libraries:
+# imported following modules and functions:
 import numpy
-import matplotlib.pyplot
-%matplotlib inline
+import matplotlib.pyplot as plt
 import pandas
 
 
-#use this function to open iris.data.csv which is saved on my device and separate all values with a coma delimiter
+#used this function to open iris.data.csv which is saved on my device and separate all values with a coma delimiter
 data = numpy.genfromtxt('iris.data.csv', delimiter=',')
 
 #listing all data from the first column of the data set which is the sepal length 
 firstcol = data[:,0]
-print(firstcol)
 
 #listing all the data from the second column of the data set which is the sepal width
 secondcol = data[:, 1]
-print(secondcol)
 
 
 
