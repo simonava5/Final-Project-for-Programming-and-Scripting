011b2451a278fcf690fcb1d80622cb8dce71dd38
Simona Vasiliauskaite <G00263352@gmit.ie>
2018-04-19 16:05:32 +0100

    Updated code on line 19 and 22 to check first and last 10 rows of data

--- Irisdata1.py
+++ Irisdata1.py
@@ -1,6 +1,7 @@
 # Simona Vasiliauskaite
 # 13/04/2018
-# Calculating the mean of each column
+# Final Project for Programming and Scripting
+# Analysis of the Iris Data Set
 
 # imported following modules and functions:
 import numpy as np
@@ -11,12 +12,14 @@ import pandas as pd
 # I used pandas to load data from this file - iris.data.cs
 data = pd.read_csv('irisdata.csv', header = 0)
 
+# Before analysing the data set, I 
+
 # quick analysis of the data
 # checking first 10 rows of the data
-data.head()
+data.head(10)
 
 # checking last 10 rows of the data
-data.tail()
+data.tail(10)
 
 # checking statistical summary of the data set
 data.describe()
